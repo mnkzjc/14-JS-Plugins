d1bca8503231a3d2cc8ee7c483d54b2241769b01
Monika Zajac <mnk.zajac@gmail.com>
2019-06-21 00:45:26 +0200

    Add carousel

---
+++ js/script.js
@@ -0,0 +1,25 @@
+'use strict'
+
+// carousel
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  autoPlay: true,
+  cellAlign: 'left',
+  contain: true,
+  wrapAround: true
+});
+
+// reset button
+var buttonReset = document.querySelector('.button-reset');
+buttonReset.addEventListener('click', function(event){
+    flkty.select(0);
+});
+
+
+// progress bar
+var progressBar = document.querySelector('.progress-bar')
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
+});
