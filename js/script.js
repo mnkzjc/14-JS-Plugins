2b244bf952813a5b44a53587ba070c0dd11c436c
Monika Zajac <mnk.zajac@gmail.com>
2019-06-29 23:38:50 +0200

    Add map

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// ----------- MUSTACHE ----------
+
 // mustache
 (function(){
   var cellTemplate = document.getElementById('carousel-cells-template').innerHTML;
@@ -9,7 +11,6 @@
   var listCells = '';
 
   for(var i = 0; i < cellData.length; i++){
-    console.log(cellData);
     listCells += Mustache.render(cellTemplate, cellData[i]);
   }
 
@@ -17,6 +18,9 @@ results.insertAdjacentHTML('beforeend', listCells);
   
 })();
 
+
+
+// ----------- FLICKITY ----------
 
 // carousel
 var elem = document.querySelector('.main-carousel');
@@ -42,4 +46,35 @@ flkty.on( 'scroll', function( progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
 });
+
+
+
+// ----------- MAPS ----------
+
+(function(){
+
+// Initialize and add the map
+  window.initMap = function () {
+
+    // The map, centered at cell-1 coords
+    var map = new google.maps.Map(
+        document.getElementById('map'), {
+          zoom: 16, 
+          center: cellData[0].coords
+        });
+
+
+    // The marker, positioned at cell-x coords
+    for(var i = 0; i < cellData.length; i++){
+      var markerX = new google.maps.Marker({
+        position: cellData[i].coords, 
+        map: map
+      });
+
+    }    
+
+  };
+
+})();
+
 
